6e72299d7fef80417a213810d795f280a0227880
Kevin Tan <tankevin7600@gmail.com>
2014-11-08 12:30:29 -0800

    Test this
    hi

---
+++ BoxDestroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;//access to Text
+public class BoxDestroy : MonoBehaviour {
+	public int health=100;
+
+	// Use this for initialization
+
+	// Update is called once per frame
+
+	void OnCollisonEnter(Collision collision){
+		Debug.Log ("I have collided with " + collision.gameObject.name);
+
+			Destroy (collision.gameObject);
+		health--;
+}
+}
