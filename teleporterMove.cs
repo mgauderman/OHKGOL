b8e1646e455e0951680e6ea37f1ad9c9ef9e19bf
Mark Gauderman <mgauderm@usc.edu>
2014-11-09 02:56:14 -0800

    TELEPORTER

---
+++ teleporterMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teleporterMove : MonoBehaviour {
+	[SerializeField] private float x = 0;                                       
+	[SerializeField] private float y = 0;                                    
+	[SerializeField] private float z = 0;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.name == "First Person Character") 
+		{
+			col.gameObject.transform.position = new Vector3(x, y, z);
+		}
+	}
+}
