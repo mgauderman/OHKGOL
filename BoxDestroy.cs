5feb0ae497b128dc42ed0c6a32e91e303be5bb86
Mark Gauderman <mgauderm@usc.edu>
2014-11-08 23:22:22 -0800

    Revert "THIS HAS ITEMS AND GAME 1 IN IT"
    This reverts commit ead95e7a335551abef808716e0ce74c2c9f92f29.

---
+++ BoxDestroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;//access to Text
+public class BoxDestroy : MonoBehaviour {
+	public int health=100;
+
+	// Use this for initialization
+
+	// Update is called once per frame
+
+	void OnCollisionEnter(Collision col){
+		Debug.Log ("I have collided with " + col.gameObject.name);
+
+			Destroy(col.gameObject);
+		health--;	
+}
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+OHKGOL
+======
+
+HackSC Game
