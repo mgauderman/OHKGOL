087f2f106e596d6674cdf3ba4e9573b6e09f3e49
Mark Gauderman <mgauderm@usc.edu>
2014-11-09 03:30:16 -0800

    betweenlevels :diamond_shape_with_a_dot_inside:

---
+++ sceneNav.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sceneNav : MonoBehaviour {
+
+	[SerializeField] private string level = "";                                       
+	
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.name == "First Person Character") 
+		{
+			Application.LoadLevel(level);
+		}
+	}
+}
