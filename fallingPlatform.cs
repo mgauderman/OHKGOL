2cf250a7f181b3240204887afe2415bd7072f979
Mark Gauderman <mgauderm@usc.edu>
2014-11-10 18:39:18 -0800

    Newest

--- fallingPlatform.cs
+++ fallingPlatform.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class fallingPlatform : MonoBehaviour {
 	
-	
+	 void Start(){
+
+		}
 	void OnTriggerEnter(Collider col){
 		Debug.Log ("I have collided with " + col.gameObject.name);
 		rigidbody.useGravity = true;
 		rigidbody.isKinematic = false;
-		if (col.gameObject.name == "Terrain") {
-			Vector3 temp = new Vector3(0,7.0f,0);
+		if (col.gameObject.tag == "terrain") {
+			Vector3 temp = new Vector3(0,147.0f,0);
 			transform.position += temp;
 			Debug.Log ("YESSS " + col.gameObject.name);
 			rigidbody.useGravity=false;
